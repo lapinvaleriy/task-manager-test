c2efb9b27e46efc96e107f306bad71ff742d9fad
lapinvaleriy <40150457+lapinvaleriy@users.noreply.github.com>
2020-10-14 23:50:35 +0300

    migration

--- project/migrations/Version20201014201224.php
+++ project/migrations/Version20201014201224.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201012094941 extends AbstractMigration
+final class Version20201014201224 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,21 +20,17 @@ final class Version20201012094941 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE task_task_statuses (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE `UTF8_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE task_tasks (id INT AUTO_INCREMENT NOT NULL, creator_id INT DEFAULT NULL, performer_id INT DEFAULT NULL, status_id INT DEFAULT NULL, title VARCHAR(255) NOT NULL, decription LONGTEXT DEFAULT NULL, created_at DATETIME NOT NULL, updated_at DATETIME DEFAULT NULL, INDEX IDX_97FC7EB761220EA6 (creator_id), INDEX IDX_97FC7EB76C6B33F3 (performer_id), UNIQUE INDEX UNIQ_97FC7EB76BF700BD (status_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE `UTF8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE task_tasks (id INT AUTO_INCREMENT NOT NULL, creator_id INT DEFAULT NULL, performer_id INT DEFAULT NULL, status VARCHAR(255) NOT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, created_at DATETIME NOT NULL, updated_at DATETIME DEFAULT NULL, INDEX IDX_97FC7EB761220EA6 (creator_id), INDEX IDX_97FC7EB76C6B33F3 (performer_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE `UTF8_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user_users (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, api_token VARCHAR(255) DEFAULT NULL, UNIQUE INDEX UNIQ_F6415EB17BA2F5EB (api_token), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE `UTF8_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE task_tasks ADD CONSTRAINT FK_97FC7EB761220EA6 FOREIGN KEY (creator_id) REFERENCES user_users (id)');
         $this->addSql('ALTER TABLE task_tasks ADD CONSTRAINT FK_97FC7EB76C6B33F3 FOREIGN KEY (performer_id) REFERENCES user_users (id)');
-        $this->addSql('ALTER TABLE task_tasks ADD CONSTRAINT FK_97FC7EB76BF700BD FOREIGN KEY (status_id) REFERENCES task_task_statuses (id)');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE task_tasks DROP FOREIGN KEY FK_97FC7EB76BF700BD');
         $this->addSql('ALTER TABLE task_tasks DROP FOREIGN KEY FK_97FC7EB761220EA6');
         $this->addSql('ALTER TABLE task_tasks DROP FOREIGN KEY FK_97FC7EB76C6B33F3');
-        $this->addSql('DROP TABLE task_task_statuses');
         $this->addSql('DROP TABLE task_tasks');
         $this->addSql('DROP TABLE user_users');
     }
